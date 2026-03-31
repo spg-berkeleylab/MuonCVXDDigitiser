93d78e8899b3e86c1efecaa10cc8fef1c9fd3c1e
Angira Rastogi <arastogi@lbl.gov>
2026-03-30 17:47:37 -0700

    Adding libgsl to the path

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ export LD_LIBRARY_PATH=${trackperf}/install/lib:${trackperf}/install/lib64:$LD_L
 export ROOT_INCLUDE_PATH=${trackperf}/install/include:$ROOT_INCLUDE_PATH
 export PYTHONPATH=${trackperf}/install/python:$PYTHONPATH
 export CMAKE_PREFIX_PATH=${trackperf}/install:$CMAKE_PREFIX_PATH
+export LD_LIBRARY_PATH=/opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/gsl-2.8-bc2rb22wiesshj5u3yod7rwo2sosqcis/lib/:$LD_LIBRARY_PATH
